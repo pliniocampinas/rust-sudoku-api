4b1364a2eb06700c9f7516c5801a9741514894cf
Plinioc <plinio.lsc@gmail.com>
2022-04-03 05:18:11 -0300

    Create hello world api

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,15 @@
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+use actix_web::{web, App, HttpServer};
 
-fn main() {
-    println!("Guess the number!");
+mod greet;
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet::handle)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
