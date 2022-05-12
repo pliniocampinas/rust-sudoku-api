92adc4cb5286ede44ad4f1d96b3058771ae3374a
Plinioc <plinio.lsc@gmail.com>
2022-05-11 22:23:48 -0300

    Extract solving function before rewriting the solve method

--- src/sudoku_solver.rs
+++ src/sudoku_solver.rs
@@ -4,15 +4,23 @@ use sudoku::Sudoku;
 // Sudokus can be created from &str's in both block or line formats or directly from bytes.
 // here, an example in line format
 // let sudoku_line = "...2...633....54.1..1..398........9....538....3........263..5..5.37....847...1...";
+fn solve(sudoku_line: String) -> Option<String> {
+
+    let sudoku_solver = Sudoku::from_str_line(sudoku_line.as_str()).unwrap();
+
+    if let Some(solution) = sudoku_solver.solve_unique() {
+        return Some(format!("{solution}"));
+    }
+
+    None
+}
 
 #[get("/solve/{sudoku_line}")]
 pub async fn handle(sudoku_line: web::Path<String>) -> impl Responder {
 
-    let sudoku_solver = Sudoku::from_str_line(sudoku_line.into_inner().as_str()).unwrap();
-
-    if let Some(solution) = sudoku_solver.solve_unique() {
+    if let Some(solution) = solve(sudoku_line.into_inner()) {
         return format!("{solution}");
     }
     
-    format!("Unsolved!")
+    format!("Invalid Sudoku")
 }
